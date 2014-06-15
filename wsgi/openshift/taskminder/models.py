f0322d812228b577867c14eee9618c4d4c7d5532
Qblack <blac2410@mylaurier.ca>
2014-06-15 19:02:21 -0400

    Updated models to reflect proper relations and loaded database with my current courses

--- wsgi/openshift/taskminder/models.py
+++ wsgi/openshift/taskminder/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Country(models.Model):
     CANADA = 'CA'
     COUNTRIES = ((CANADA,'Canada'),)
     name = models.CharField(max_length=200,choices=COUNTRIES)
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Province(models.Model):
@@ -14,58 +15,61 @@ class Province(models.Model):
     name = models.CharField(max_length=20,choices=PROVINCE_CHOICES)
     country = models.OneToOneField(Country)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class University(models.Model):
     name = models.CharField(max_length=200)
     province = models.OneToOneField(Province)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Professor(models.Model):
     name = models.CharField(max_length=200)
     universities = models.ManyToManyField(University)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Course(models.Model):
     course_name = models.CharField(max_length=200)
     course_code = models.CharField(max_length=10)
     course_section = models.CharField(max_length=10)
-    professor = models.OneToOneField(Professor)
+    professor = models.ManyToManyField(Professor)
 
-    def __unicode__(self):
-        return self.course_name+' '+self.course_section
+
+    def __str__(self):
+        return self.course_name+'-'+self.course_section
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
-    due_date = models.DateTimeField('due date')
-    course = models.OneToOneField(Course)
+    due_date = models.DateField('due date')
+    time = models.TimeField(default=None,blank=True,null=True)
+    course = models.ForeignKey(Course)
     completed = models.BooleanField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 class Reading(models.Model):
     title = models.CharField(max_length=200)
-    due_date = models.DateTimeField('due date')
-    course = models.OneToOneField(Course)
-    pages = models.CharField(max_length=20)
+    due_date = models.DateField('due date')
+    course = models.ForeignKey(Course)
+    pages = models.CharField(max_length=20,null=True,blank=True)
     completed = models.BooleanField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 class Test(models.Model):
     title = models.CharField(max_length=200)
-    due_date = models.DateTimeField('due date')
-    course = models.OneToOneField(Course)
+    due_date = models.DateField('due date')
+    time = models.TimeField(default=None,blank=True,null=True)
+    course = models.ForeignKey(Course)
     completed = models.BooleanField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 

70b0c44446bdae7a491cf61aabc43a4546728d48
Qblack <blac2410@mylaurier.ca>
2014-06-15 11:37:52 -0400

    Added additional models to tasks

--- wsgi/openshift/tasks/admin.py
+++ wsgi/openshift/tasks/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from tasks.models import Assignment, Reading, Test
-
+from tasks.models import Assignment, Reading, Test, Course, Professor, University, Country, Province
 
 # Register your models here.
-
 
 admin.site.register(Assignment)
 admin.site.register(Reading)
 admin.site.register(Test)
+admin.site.register(Course)
+admin.site.register(Professor)
+admin.site.register(University)
+admin.site.register(Province)
--- wsgi/openshift/tasks/models.py
+++ wsgi/openshift/tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateTimeField('due date')
-    course_code = models.CharField(max_length=10)
+    course = models.OneToOneField(Course)
 
     def __unicode__(self):
         return self.title
@@ -12,7 +12,7 @@ class Assignment(models.Model):
 class Reading(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateTimeField('due date')
-    course_code = models.CharField(max_length=10)
+    course = models.OneToOneField(Course)
     pages = models.CharField(max_length=20)
 
     def __unicode__(self):
@@ -21,8 +21,49 @@ class Reading(models.Model):
 class Test(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateTimeField('due date')
-    course_code = models.CharField(max_length=10)
+    course = models.OneToOneField(Course)
 
     def __unicode__(self):
         return self.title
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    course_code = models.CharField(max_length=10)
+    course_section = models.CharField(max_length=10)
+    professor = models.OneToOneField(Professor)
+
+    def __unicode__(self):
+        return self.course_name+' '+self.course_section
+
+class Professor(models.Model):
+    name = models.CharField(max_length=200)
+    universities = models.ManyToManyField(University)
+
+    def __unicode__(self):
+        return self.name
+
+
+class University(models.Model):
+    name = models.CharField(max_length=200)
+    province = models.OneToOneField(Province)
+
+    def __unicode__(self):
+        return self.name
+
+class Country(models.Model):
+    SCHOOLS = ('Canada',)
+    name = models.CharField(max_length=200,choices=SCHOOLS)
+    def __unicode__(self):
+        return self.name
+
+class Province(models.Model):
+    PROVINCE_CHOICES = ('Ontario',)
+    name = models.CharField(max_length=20,choices=PROVINCE_CHOICES)
+    country = models.OneToOneField(Country)
+
+    def __unicode__(self):
+        return self.name
+
+
 

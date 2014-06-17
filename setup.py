12628690d09c57e46abeabedd72235a8a8f4d8ac
Qblack <blac2410@mylaurier.ca>
2014-06-16 20:27:36 -0400

    Added django-bootstrap3'

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import os
 packages = ['Django<=1.6',
             'static3',  # If you want serve the static files in the same server
             'south',
+            'django-bootsrap3',
             # 'mysql-connector-python',
             # 'pymongo',
             # 'psycopg2',
